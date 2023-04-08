32c3ba9799a9ced06a44599548e28a7be0fec690
Peter <qqq5288888@gmail.com>
2023-04-08 13:29:08 +0800

    modify

--- lesson3.py
+++ lesson3.py
@@ -1,6 +1,6 @@
 import private
 import requests
-url = f'https://maker.ifttt.com/trigger/button_press1/with/key/{private.ifttt}?value1=31&value2=51'
+url = f'https://maker.ifttt.com/trigger/button_press/with/key/{private.ifttt}?value1=31&value2=51'
 print(private.ifttt)
 #print(url)
 r = requests.get(url)
