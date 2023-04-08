db69a62bcc6d486bb9e8e289d32aebfd384c3a66
Peter <qqq5288888@gmail.com>
2023-04-08 12:56:47 +0800

    modify

--- lesson3.py
+++ lesson3.py
@@ -1,2 +1,8 @@
 import private
+import requests
+url = f'https://maker.ifttt.com/trigger/button_press1/with/key/{private.ifttt}?value1=31&value2=51'
 print(private.ifttt)
+#print(url)
+r = requests.get(url)
+if r.status_code == 200:
+    print("發生成功")
